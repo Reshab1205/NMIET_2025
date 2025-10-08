1f2fe1e5eb70c36029db57810d33878721a3fac8
Reshab1205 <rkmodi12051998@gmail.com>
2025-10-08 12:22:26 +0530

    debug success

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,12 +1,34 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+const user = require("../models/userSchema");
 
 const createToken = (userId, email) => {
-    return jwt.sign({id: userId, email:email}, process.env.JWT_SECRET_KEY, {expiresIn:process.env.JWT_EXPIRE_TIME})
-}
+  return jwt.sign({ id: userId, email: email }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRE_TIME,
+  });
+};
 
-const verifyToken = () => {
+const verifyToken = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+      return res.status(401).json({ message: "UnAuthorized Access" });
+    }
+    const token = authHeader.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "No Token" });
 
-}
 
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    
+    const getUser = await user.findById(decoded.id).select("-password");
+    console.log('getUser', getUser)
+    if (!getUser) return res.status(401).json({ message: "User not found" });
+    req.user = user;
+    next();
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: err });
+  }
+};
 
-module.exports = {createToken, verifyToken}
+module.exports = { createToken, verifyToken };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const productController = require('../controllers/productController')
+const { verifyToken } = require('../middlewares/authMiddleware')
 
 const router = express.Router()
 
-router.post('/add-product', productController.createProduct)
+router.post('/add-product', verifyToken, productController.createProduct)
 router.get('/get-products', productController.getAllProducts)
 // update
 // soft delete
